68335751b0b76320c3796554b29a501d762b2e0a
Onwuasoanya James <onwuasoanyajames@gmail.com>
2022-07-03 22:14:36 +0100

    progress

--- countdowntimer.js
+++ countdowntimer.js
@@ -21,9 +21,6 @@
                 if(input[i].value.length==0||input[i].value==0){
                     input[i].value='00'
                 }
-                if(input[i].value>0 && input[i].value.length<2){
-
-                }
                 runValue[i].textContent= input[i].value.padStart(2,'0')
             }
         }
@@ -32,8 +29,15 @@
         }
     })
 
-    function moveon(num){
+   input.forEach(i=>{
+        i.onfocus=()=>{
+            if(!i.hasAttribute('disabled')){
+                i.value=''
+            }
+        }
+   })
 
+    function moveon(num){
         const target = input[num]
         let ivalue= target.value.length
         let nextInputIndex = input[num+1]
@@ -51,29 +55,58 @@
             nextInputIndex.focus()
         } else if(ivalue==2 && nextInputIndex==noIndex){
 
-            input.forEach(box=>{
-                if(box.length==0){
-                    console.log('yipaaaaaaa')
-                }})
-            set.focus()            
+        set.focus()            
         }
         return
-        
     }
-
-
 
     startORpause.addEventListener('click',e=>{
 
+        startORpause.classList.toggle('running')
+        // if(startORpause.classList.contains('running')){
+
+            let dayValue = runValue[0].textContent*86400000
+            let hourValue = runValue[1].textContent*3600000
+            let minuteValue = runValue[2].textContent*60000
+            let secondValue = runValue[3].textContent*1000
+            const allValue = dayValue+hourValue+minuteValue+secondValue
+            const present = new Date().getTime()
+            const future = new Date(present + allValue)
+
+            
+            if(allValue!=0){    
+            let counter = setInterval(()=>{ 
+                const now = new Date()
+                const countDownTime = future.getTime()- now.getTime()
+                const seconds = 1000;
+                const minutes = seconds*60 ;
+                const hours = minutes* 60;
+                const days = hours *24; 
+            
+                const day =Math.floor(countDownTime/days)
+                const hour =Math.floor((countDownTime%days)/hours)
+                const minute =Math.floor((countDownTime%hours)/minutes)
+                const second =Math.floor((countDownTime%minutes)/seconds);
+            
+                runValue[0].textContent=day.toString().padStart(2,'0')
+                runValue[1].textContent=hour.toString().padStart(2,'0')
+                runValue[2].textContent=minute.toString().padStart(2,'0')
+                runValue[3].textContent=second.toString().padStart(2,'0')
+
+                if (countDownTime!=0 && !startORpause.classList.contains('running')){
+                    clearInterval(counter)
+                    second++
+                }
+                else if (countDownTime<=0){
+                    runValue[0].textContent='00'
+                    runValue[1].textContent='00'
+                    runValue[2].textContent='00'
+                    runValue[3].textContent='00'
+                    clearInterval(counter)
+                }
+            },1000)
+            } else {
+               set.focus()
+            }
        
-
-        // let dayValue = input[0].value
-        // let hourValue = input[1].value
-        // let minuteValue = input[2].value
-        // let secondValue = input[3].value
-
-       
-
-        const date= new Date()
-        
     })
